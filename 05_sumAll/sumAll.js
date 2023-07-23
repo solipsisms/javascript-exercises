197e081f3a8295c2d331da4e0286395fcef7ade8
solipsisms <120543057+solipsisms@users.noreply.github.com>
2023-07-23 11:51:25 -0500

    solved #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(firstInteger, secondInteger) {
+    if (!Number.isInteger(firstInteger) || !Number.isInteger(secondInteger)) {
+        return 'ERROR';
+    } else if (firstInteger < 0 || secondInteger < 0) {
+        return 'ERROR';
+    } else {
+        let sum = 0;
+        if (firstInteger < secondInteger) {
+            for (let i = firstInteger; i <= secondInteger; i++) {
+                sum += i;
+            }
+        } else {
+            for (let i = secondInteger; i <= firstInteger; i++) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
