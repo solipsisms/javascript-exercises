fec10658663ac396e8f640417f52eb4969c909d8
solipsisms <120543057+solipsisms@users.noreply.github.com>
2023-07-23 12:12:04 -0500

    solved #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let convertedC = (tempInF - 32) * (5/9);
+  convertedC = Math.round(convertedC * 10) / 10;
+  return convertedC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let convertedF = (tempInC * (9/5) + 32);
+  convertedF = Math.round(convertedF * 10) / 10;
+  return convertedF;
 };
 
 // Do not edit below this line
