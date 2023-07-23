ef889e89d4463f902d5268c8d2b9fe2e527247d3
solipsisms <120543057+solipsisms@users.noreply.github.com>
2023-07-23 12:05:12 -0500

    solved #6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeap = false; 
+    if (!Number.isInteger(year) || year < 0) {
+        return 'ERROR';
+    } else {
+        if (year%2 === 0) {
+            isLeap = true;
+        } 
+        if (year%100 === 0) {
+            if (year%400 === 0) {
+                isLeap = true;
+            } else {
+                isLeap = false;
+            }
+        }
+    }
+    return isLeap;
 };
 
 // Do not edit below this line
