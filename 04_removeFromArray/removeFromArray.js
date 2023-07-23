81badfc2392a211b0ae64657cbc7fdccbaea0641
solipsisms <120543057+solipsisms@users.noreply.github.com>
2023-07-23 11:43:37 -0500

    solved #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...valuesToRemove) { //... is the rest parameter allowing multiple values to be taken in
+    return array.filter((item) => !valuesToRemove.includes(item));
 };
 
 // Do not edit below this line
