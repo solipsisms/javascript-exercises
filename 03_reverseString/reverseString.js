b6ae84ca36afc2eefc1792eeca9d62e0a02dc363
solipsisms <120543057+solipsisms@users.noreply.github.com>
2023-07-23 11:19:53 -0500

    solved #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringInArray = [];
+    for (let i = 0; i <= string.length; i++) {
+        stringInArray.push(string[i]);
+    }
+    let stringInReverse = "";
+    for (let j = (string.length - 1); j >= 0; j--) {
+        stringInReverse += stringInArray[j];
+    }
+    return stringInReverse;
 };
 
 // Do not edit below this line
