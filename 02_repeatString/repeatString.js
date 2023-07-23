47f300a7618189c0a78309f2fb463477a1d17195
solipsisms <120543057+solipsisms@users.noreply.github.com>
2023-07-22 22:57:14 -0500

    solved #2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        let returnVal = "";
+        for (let i = 0; i < num; i++) {
+            returnVal += string;
+        }
+    return returnVal;
+    }
 };
 
 // Do not edit below this line
